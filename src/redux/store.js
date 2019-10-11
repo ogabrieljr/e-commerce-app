2dadddbef56233455d71b2a91fcade1c112367db
Gabriel <jgj_junior@yahoo.com.br>
2019-10-11 00:05:53 -0300

    added logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
+import logger from "redux-logger";
+
+const middlewares = [logger];
 
 const rootReducer = combineReducers({ userReducer, cartReducer });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
