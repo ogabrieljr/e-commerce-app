933dc045c620de3b1b7ec52015f69dc4b1af9d9a
Gabriel <jgj_junior@yahoo.com.br>
2019-10-13 08:09:19 -0300

    finished additem function

--- src/redux/cart/cartUtils.js
+++ src/redux/cart/cartUtils.js
@@ -1,9 +1,9 @@
-/*
-items:[]
-itemtoadd: {}
-*/
-
 export const addItemToCart = (items, itemToAdd) => {
-  //...code
-  return [...items, { ...itemToAdd, quantity: 1 }];
+  const existingItem = items.find(item => item.id === itemToAdd.id);
+  if (existingItem) {
+    return items.map(item => {
+      if (item.id === itemToAdd.id) return { ...itemToAdd, quantity: item.quantity + 1 };
+      else return item;
+    });
+  } else return [...items, { ...itemToAdd, quantity: 1 }];
 };
