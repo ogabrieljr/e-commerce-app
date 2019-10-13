caf67b005abd6f3bca8504af5738a77f8c92c39d
Gabriel <jgj_junior@yahoo.com.br>
2019-10-13 08:15:21 -0300

    changed conditional to ternary

--- src/redux/cart/cartUtils.js
+++ src/redux/cart/cartUtils.js
@@ -1,9 +1,8 @@
 export const addItemToCart = (items, itemToAdd) => {
   const existingItem = items.find(item => item.id === itemToAdd.id);
   if (existingItem) {
-    return items.map(item => {
-      if (item.id === itemToAdd.id) return { ...itemToAdd, quantity: item.quantity + 1 };
-      else return item;
-    });
+    return items.map(item =>
+      item.id === itemToAdd.id ? { ...itemToAdd, quantity: item.quantity + 1 } : item
+    );
   } else return [...items, { ...itemToAdd, quantity: 1 }];
 };
