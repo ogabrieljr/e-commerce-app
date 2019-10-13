e059965798242eff6a001083eeae45eb3b3414b4
Gabriel <jgj_junior@yahoo.com.br>
2019-10-13 18:14:26 -0300

    connecting items to dropdown

--- src/components/cart dropdown/Dropdown.js
+++ src/components/cart dropdown/Dropdown.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Button from "../button/button";
 import "./style.scss";
+import { connect } from "react-redux";
 
-export default function Dropdown() {
+function Dropdown({ items }) {
   return (
     <div className="cart-dropdown">
       <div className="cart-items" />
+      {console.log(items)}
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  items: state.cartReducer.items
+});
+
+export default connect(mapStateToProps)(Dropdown);
