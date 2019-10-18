157e6d2569a034ee6e46d15950bb00e22938b5d0
Gabriel <jgj_junior@yahoo.com.br>
2019-10-18 20:09:14 -0300

    implementing redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,45 +2,20 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
 // import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-const middlewares = [];
+const persistConfig = {
+  key: "root",
+  storage
+};
 
 const rootReducer = combineReducers({ userReducer, cartReducer });
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default store;
+const middlewares = [];
 
-// // configureStore.js
+export const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 
-// import { createStore } from 'redux'
-// import { persistStore, persistReducer } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-
-// import rootReducer from './reducers'
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// }
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-// export default () => {
-//   let store = createStore(persistedReducer)
-//   let persistor = persistStore(store)
-//   return { store, persistor }
-// }
-
-// import { PersistGate } from 'redux-persist/integration/react'
-
-// // ... normal setup, create store and persistor, import components etc.
-
-// const App = () => {
-//   return (
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistor}>
-//         <RootComponent />
-//       </PersistGate>
-//     </Provider>
-//   );
+export const persistor = persistStore(store);
