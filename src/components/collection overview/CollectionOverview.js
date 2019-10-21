6520a0bac59d7be621199d0b73702561255353dd
Gabriel <jgj_junior@yahoo.com.br>
2019-10-21 19:57:37 -0300

    logging collections

--- src/components/collection overview/CollectionOverview.js
+++ src/components/collection overview/CollectionOverview.js
@@ -4,17 +4,18 @@ import { connect } from "react-redux";
 import { getShopCollections } from "../../redux/shop/shopSelector";
 
 function CollectionOverview({ collections }) {
+  console.log(collections)
   return (
     <div>
-      {collections.map(({ id, ...otherProps }) => (
+      {/* {collections.map(({ id, ...otherProps }) => (
         <PreviewCollection key={id} {...otherProps} />
-      ))}
+      ))} */}
     </div>
   );
 }
 
 const mapStateToProps = state => ({
-  collections: getShopCollections(state)
+  collections: state.shopReducer.collections
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
