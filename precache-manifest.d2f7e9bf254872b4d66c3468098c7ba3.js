19bfe61403452442d7537978b43894ce28765318
Gabriel <jgj_junior@yahoo.com.br>
2019-10-24 01:28:26 -0300

    Updates

---
+++ precache-manifest.d2f7e9bf254872b4d66c3468098c7ba3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cc4e791aa5dfb279770c218bed4549",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "e151c9d457cb79a78d70",
+    "url": "/gh-pages/static/css/main.19155961.chunk.css"
+  },
+  {
+    "revision": "694d08aa591f9c4d3ccc",
+    "url": "/gh-pages/static/js/2.b220d27b.chunk.js"
+  },
+  {
+    "revision": "e151c9d457cb79a78d70",
+    "url": "/gh-pages/static/js/main.5715a482.chunk.js"
+  },
+  {
+    "revision": "e6ab738ec75f22171ce2",
+    "url": "/gh-pages/static/js/runtime-main.96fc504b.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/gh-pages/static/media/cart icon.30f07071.svg"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/gh-pages/static/media/crown.19ef7b17.svg"
+  }
+]);
