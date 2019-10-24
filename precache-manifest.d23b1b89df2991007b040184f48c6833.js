a924cfb60e2f1aa3726bc7677723bbc31043a36f
Gabriel <jgj_junior@yahoo.com.br>
2019-10-24 02:11:30 -0300

    Updates

---
+++ precache-manifest.d23b1b89df2991007b040184f48c6833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28915a92cab72cf31c81bb02b57cbc8c",
+    "url": "/e-commerce-crw/index.html"
+  },
+  {
+    "revision": "d106c5ded7bd14d81fa1",
+    "url": "/e-commerce-crw/static/css/main.19155961.chunk.css"
+  },
+  {
+    "revision": "3508df65dd10c7d338e4",
+    "url": "/e-commerce-crw/static/js/2.6661689f.chunk.js"
+  },
+  {
+    "revision": "d106c5ded7bd14d81fa1",
+    "url": "/e-commerce-crw/static/js/main.cf629b18.chunk.js"
+  },
+  {
+    "revision": "6ddf4b53bf704b165253",
+    "url": "/e-commerce-crw/static/js/runtime-main.bf3cd58b.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce-crw/static/media/cart icon.30f07071.svg"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/e-commerce-crw/static/media/crown.19ef7b17.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d761c9ba31a74808871d7aa1c104d06f.js"
+  "/e-commerce-crw/precache-manifest.d23b1b89df2991007b040184f48c6833.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-commerce-crw/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
