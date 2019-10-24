27f28fac7a8a0c93575aeaf5f734e3542c63f51a
Gabriel <jgj_junior@yahoo.com.br>
2019-10-24 02:42:15 -0300

    update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter  basename={process.env.PUBLIC_URL}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
