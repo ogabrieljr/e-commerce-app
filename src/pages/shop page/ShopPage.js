bcffba1a46bc8c614beef0bcc6d4a554c0b33f86
Gabriel <jgj_junior@yahoo.com.br>
2019-10-29 22:12:47 -0300

    shop page to class

--- src/pages/shop page/ShopPage.js
+++ src/pages/shop page/ShopPage.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import CollectionOverview from "../../components/collection overview/CollectionOverview";
 import { Route } from "react-router-dom";
 import Collection from "../collection/Collection";
 
-export default function ShopPage({ match }) {
-  return (
-    <div>
-      <Route exact path={`${match.url}`} component={CollectionOverview} />
-      <Route path={`${match.url}/:collectionid`} component={Collection} />
-    </div>
-  );
+class ShopPage extends Component {
+  render() {
+    const { match } = this.props;
+    return (
+      <div>
+        <Route exact path={`${match.url}`} component={CollectionOverview} />
+        <Route path={`${match.url}/:collectionid`} component={Collection} />
+      </div>
+    );
+  }
 }
+
+export default ShopPage;
