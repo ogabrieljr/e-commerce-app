95e7dd9958c6173a1beecd308fc3f93a942ec226
Gabriel <jgj_junior@yahoo.com.br>
2019-10-31 20:14:30 -0300

    fetch collections from firebase,  mapped, reduced collections adn update shop reducer

--- src/pages/shop page/ShopPage.js
+++ src/pages/shop page/ShopPage.js
@@ -2,8 +2,31 @@ import React, { Component } from "react";
 import CollectionOverview from "../../components/collection overview/CollectionOverview";
 import { Route } from "react-router-dom";
 import Collection from "../collection/Collection";
+import { firestore } from "../../firebase/Firebase";
+import { connect } from "react-redux";
+import { updateCollections } from "../../redux/shop/shopActions";
 
 class ShopPage extends Component {
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+    collectionRef.get().then(items => {
+      const mappedItems = items.docs.map(item => {
+        const { title, items } = item.data();
+        return {
+          title,
+          items,
+          id: item.id,
+          routeName: encodeURI(title.toLowerCase())
+        };
+      });
+      const reducedItems = mappedItems.reduce((acc, items) => {
+        acc[items.title.toLowerCase()] = items;
+        return acc;
+      }, {});
+      this.props.updateCollections(reducedItems);
+    });
+  }
+
   render() {
     const { match } = this.props;
     return (
@@ -15,4 +38,11 @@ class ShopPage extends Component {
   }
 }
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collections => dispatch(updateCollections(collections))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
--- src/redux/shop/shopReducer.js
+++ src/redux/shop/shopReducer.js
@@ -1,11 +1,14 @@
-import SHOP_DATA from "./shopData";
-
 const INITIAL_STATE = {
-  collections: SHOP_DATA
+  collections: ""
 };
 
-const shopReducer = (state = INITIAL_STATE) => {
-  return state;
+const shopReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "UPDATE_COLLECTIONS":
+      return { ...state, collections: action.payload };
+    default:
+      return state;
+  }
 };
 
 export default shopReducer;
