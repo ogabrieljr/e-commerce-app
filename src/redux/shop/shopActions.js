4a479ff9f5e1267e4a66cfeb45caf858de2a66e1
Gabriel <jgj_junior@yahoo.com.br>
2019-11-03 20:49:20 -0200

    making a start case function for future instance just in case

--- src/redux/shop/shopActions.js
+++ src/redux/shop/shopActions.js
@@ -1,5 +1,9 @@
 import { firestore } from "../../firebase/Firebase";
 
+export const fetchCollectionsStart = () => ({
+  type: "FETCH_COLLECTIONS_START"
+});
+
 export const fetchCollectionsSuccess = collections => ({
   type: "FETCH_COLLECTIONS_SUCCESS",
   payload: collections
--- src/redux/shop/shopReducer.js
+++ src/redux/shop/shopReducer.js
@@ -5,6 +5,8 @@ const INITIAL_STATE = {
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case "FETCH_COLLECTIONS_START":
+      return { ...state };
     case "FETCH_COLLECTIONS_SUCCESS":
       return { ...state, collections: action.payload };
     case "FETCH_COLLECTIONS_FAIL":
