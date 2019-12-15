36e4cb7e95fcfa0e14904bb6af2449ec211de1da
Gabriel <jgj_junior@yahoo.com.br>
2019-12-15 01:17:27 -0200

    removed @import for link import

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,7 +3,6 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 
 body {
-  @import url('https://fonts.googleapis.com/css?family=Just+Another+Hand|Open+Sans&display=swap');
   font-family: 'Open Sans', sans-serif;
   font-size: 13px;
   padding: 20px 60px;
