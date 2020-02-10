c31185d20fe78f6a6bb0ab77138bbed203b696e2
whatisthenote <jgj_junior@yahoo.com.br>
2020-02-09 22:03:46 -0300

    form test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, mount, render } from "enzyme";
+import React from "react";
+import Forms from "./pages/sign in/Forms";
+import App from "../src/App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+it("render", () => {
+  expect(shallow(<Forms />)).toMatchSnapshot();
 });
